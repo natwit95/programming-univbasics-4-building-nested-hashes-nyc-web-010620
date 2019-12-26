7b3b44910fad5afc5f5efb539e244c4810e0cb84
Natalia  Wit <natwit95@gmail.com>
2019-12-26 03:25:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,7 @@ def monopoly_with_third_tier
   hash = {:railroads => {:rent_in_dollars => {
     :one_piece_owned => 25,
     :two_pieces_owned => 50,
-    :price3 => 8,
+    :three_pieces_owned => 100,
     :price4 => 9
     },
     :names => {
